d87c03a70231987fd8ba4b75455c315907686cdd
A.J. May <amay@northps.com>
2015-02-27 17:09:16 -0500

    Initial Commit

---
+++ web.js
@@ -0,0 +1,19 @@
+var ack = require('ac-koa').require('hipchat');
+var pkg = require('./package.json');
+var app = ack(pkg);
+
+var addon = app.addon()
+  .hipchat()
+  .allowRoom(true)
+  .scopes('send_notification');
+
+addon.webhook('room_message', /@[Dd]orman/, function *() {
+  var phrases = [
+    "I left the company to pursue my dreams. If you need to reach me, I'll be sitting on my couch watching TV.",
+    "I love Druuuu-pall!"
+  ];
+
+  yield this.roomClient.sendNotification(phrases[Math.floor(Math.random() * phrases.length)]);
+});
+
+app.listen();
