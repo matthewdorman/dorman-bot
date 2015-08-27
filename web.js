84465e91326ac8fab41f0c4770977eb14ef0f014
Matthew Dorman <matthewdorman@gmail.com>
2015-08-27 11:31:40 -0600

    someone deleted a comma,

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ var addon = app.addon()
 
 addon.webhook('room_message', /@[Dd]orman/, function *() {
   var phrases = [
-    "Meet me at Prontos 8/27/2015 at 5:00 PM Eastern sharp."
+    "Meet me at Prontos 8/27/2015 at 5:00 PM Eastern sharp.",
     "I left the company to pursue my dreams. If you need to reach me, I'll be sitting on my couch drinking whiskey and yelling at my TV.",
     "Let me google that for you.",
     "WordWhat?",
